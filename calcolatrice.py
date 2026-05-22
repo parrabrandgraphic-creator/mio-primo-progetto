f16360952db38b9756e59b20493a6c122c97e743
Alberto <tua@email.comparrabrandgraphic@gmail.com>
2026-05-22 16:57:17 +0200

    Aggiunto file calcolatrice

---
+++ calcolatrice.py
@@ -0,0 +1,17 @@
+def somma(a, b):
+    return a + b
+
+def sottrai(a, b):
+    return a - b
+
+def moltiplica(a, b):
+    return a * b
+
+def dividi(a, b):
+    return a / b
+
+risultato = somma(10, 5)
+print("Somma:", risultato)
+
+risultato2 = dividi(10, 0)
+print("Divisione:", risultato2)
